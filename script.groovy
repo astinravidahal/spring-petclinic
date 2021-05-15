955c17159f3ada155b6711f19bff9f7b1432e915
astinravidahal <44469638+astin2044327@users.noreply.github.com>
2021-05-15 12:46:51 -0400

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -3,8 +3,9 @@ def runTest() {
 }
 def finalRun() {
   println 'this is the finalRun function' 
+}
   def runParam() {
     echo "let check if this works ${params.My_String} "
   }
-}
+
 return this 
